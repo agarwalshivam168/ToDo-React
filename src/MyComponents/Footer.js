452cb4617e197104a2ee0f849669d5d661b47090
agarwal168 <agarwalshivam168@gmail.com>
2023-01-25 23:38:40 +0530

    push

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import './footer.css'
+
+const Footer = () => {
+  // let footerStyle = {
+  //   position: "relative",
+  //   top: "10vh",
+  //   width: "100%"
+  // }
+
+  return (
+    <footer className='bg-dark text-light'>
+      <p className='text-center py-3'>Copyright &copy; MyTodosList.com</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {TodoItem} from './TodoItem';
+
+const Todos = (props) => {
+  let myStyle = {
+    height: "70vh"
+  }
+  return (
+    <div className='container my-3' style={myStyle}>
+      <h3 className='my-3'>Todos List</h3>
+      {props.todos.length === 0 ? "No Todos to display" :
+      props.todos.map((todo)=>{
+        return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+      })}
+    </div>
+  )
+}
+
+export default Todos
